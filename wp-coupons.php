5f31957f709cb923dc7aaefd7bf55c2e352b8465
Josef Wittmann <josef.wittmann@tutanota.com>
2020-12-03 10:25:48 +0100

    :building_construction: Scaffold plugin with WP-CLI

---
+++ wp-coupons.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     WP Coupons
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Distribute unique coupons to your users
+ * Author:          Josef Wittmann <josef.wittmann@tutanota.com>
+ * Author URI:      https://josefwittmann.dev
+ * Text Domain:     wp-coupons
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Coupons
+ */
