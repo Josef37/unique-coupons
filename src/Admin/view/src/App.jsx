10a3d1ae1f9fbbe3783b18f3cce053718ad22bfd
Josef Wittmann <josef.wittmann@tutanota.com>
2020-12-04 20:56:29 +0100

    :children_crossing: Add navigation tabs

--- src/Admin/view/src/App.jsx
+++ src/Admin/view/src/App.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import CouponGroup from './pages/coupon-group.component';
 import AddCoupons from './pages/add-coupons.component';
 import Dashboard from './pages/dashboard.component';
 import Settings from './pages/settings.component';
+import NavTabs from './components/nav-tabs.component';
 
 function App() {
 	return <div>
-		<ul>
-			<li><Link to="/">Dashboard</Link></li>
-			<li><Link to="/settings">Settings</Link></li>
-		</ul>
+		<NavTabs tabs={[
+			{ path: '/', label: 'Dashboard' },
+			{ path: '/settings', label: 'Settings' }
+		]} />
 		<Switch>
-			<Route path="/settings">
+			<Route exact path="/settings">
 				<Settings />
 			</Route>
-			<Route path="/group/:groupId" render={({ match }) =>
+			<Route exact path="/group/:groupId" render={({ match }) =>
 				<CouponGroup groupId={match.params.groupId} />
 			} />
-			<Route path="/add-coupons/:groupId" render={({ match }) =>
+			<Route exact path="/add-coupons/:groupId" render={({ match }) =>
 				<AddCoupons groupId={match.params.groupId} />
 			} />
-			<Route>
+			<Route exact path="/">
 				<Dashboard />
+			</Route>
+			<Route>
+				<div>Invalid route</div>
 			</Route>
 		</Switch>
 	</div>;
