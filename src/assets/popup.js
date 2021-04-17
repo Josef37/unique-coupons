6c347bf8cb35e89e9c6d99644905c96988fb5cb2
Josef Wittmann <josef.wittmann@tutanota.com>
2021-04-17 06:46:41 +0200

    Add popup to frontend

--- src/assets/popup.js
+++ src/assets/popup.js
@@ -8,47 +8,102 @@ Responsible for
 Maybe add all selectors and stuff into localization
 */
 
-window.addEventListener("DOMContentLoaded", (event) => {
-	const popup = document.querySelector(".wp-coupons-popup");
-	initPopup(popup);
-});
-
-function initPopup(popup) {
-	const { groupId } = popup.dataset;
-	if (!groupId) return;
-
-	addButtonListeners(popup, groupId);
-	queuePopupDisplaying(popup);
-}
-
-function addButtonListeners(popup, groupId) {
-	popup.querySelectorAll(".wp-coupons-popup__button").forEach((button) => {
-		button.addEventListener("click", () => handleRetrieval(groupId));
+{
+	window.addEventListener("DOMContentLoaded", (event) => {
+		const popupElement = document.querySelector(".wp-coupons-popup");
+		new Popup(popupElement);
 	});
-}
 
-function handleRetrieval(groupId) {
-	fetch(wpCouponsPopup.api.retrieveCoupon, {
-		method: "POST",
-		body: JSON.stringify({
-			group_id: groupId,
-		}),
-		headers: {
-			"Content-Type": "application/json",
-			"X-WP-Nonce": wpCouponsPopup.api.nonce,
-		},
-	})
-		.then((response) => response.json())
-		.then((json) => {
-			document.querySelector(".wp-coupons-popup__coupon").innerHTML =
-				'<pre style="font-size: 2rem;">' +
-				JSON.stringify(json, null, 2) +
-				"</pre>";
-		});
-}
+	class Popup {
+		elements = {};
+		groupId;
+		canFetch = true;
 
-function queuePopupDisplaying(popup) {
-	setTimeout(() => {
-		popup.style.display = "block";
-	}, wpCouponsPopup.timeoutInMs);
+		constructor(element) {
+			this.elements.container = element;
+			this.init();
+		}
+
+		init = () => {
+			this.groupId = this.elements.container.dataset.groupId;
+			if (!this.groupId) return;
+
+			this.selectDomElements();
+			this.addButtonListeners();
+			this.queuePopup();
+		};
+
+		selectDomElements = () => {
+			this.elements.buttons = this.elements.container.querySelectorAll(
+				".wp-coupons-popup__button"
+			);
+			this.elements.coupon = this.elements.container.querySelector(
+				".wp-coupons-popup__coupon"
+			);
+			this.elements.value = this.elements.container.querySelector(
+				".wp-coupons-popup__value"
+			);
+			this.elements.expiresAt = this.elements.container.querySelector(
+				".wp-coupons-popup__expires_at"
+			);
+		};
+
+		addButtonListeners = () => {
+			this.elements.buttons.forEach((button) => {
+				button.addEventListener("click", () => this.handleRetrieval());
+			});
+		};
+
+		handleRetrieval = () => {
+			if (!this.canFetch) return;
+			this.setCanFetch(false);
+
+			this.fetchCoupon()
+				.then(this.checkResponseStatus)
+				.then((response) => response.json())
+				.then(this.showCoupon)
+				.catch(this.handleResponseError);
+		};
+
+		fetchCoupon = () => {
+			return fetch(wpCouponsPopup.api.retrieveCoupon, {
+				method: "POST",
+				body: JSON.stringify({
+					group_id: this.groupId,
+				}),
+				headers: {
+					"Content-Type": "application/json",
+					"X-WP-Nonce": wpCouponsPopup.api.nonce,
+				},
+			});
+		};
+
+		checkResponseStatus = (response) => {
+			if (response.ok) return response;
+			throw new Error("Response not ok");
+		};
+
+		showCoupon = ({ value, expires_at }) => {
+			this.elements.coupon.style.display = "block";
+			this.elements.value.textContent = value;
+			this.elements.expiresAt.textContent = expires_at;
+		};
+
+		handleResponseError = () => {
+			this.setCanFetch(true);
+		};
+
+		queuePopup = () => {
+			setTimeout(() => {
+				this.elements.container.style.display = "block";
+			}, wpCouponsPopup.timeoutInMs);
+		};
+
+		setCanFetch(canFetch) {
+			this.canFetch = canFetch;
+			this.elements.buttons.forEach((button) => {
+				button.disabled = !canFetch;
+			});
+		}
+	}
 }
