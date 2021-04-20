0109e5764309fcc647384b588b8ab7d8503489f9
Josef Wittmann <josef.wittmann@tutanota.com>
2021-04-20 22:14:55 +0200

    Fix options test

--- tests/src/OptionsTest.php
+++ tests/src/OptionsTest.php
@@ -11,12 +11,21 @@ class OptionsTest extends WP_UnitTestCase {
 	}
 
 	public function test_set_option() {
-		list( $key, $option ) = array( 'time_between_any_popup', 10 );
+		list( $key, $option ) = array( 'seconds_between_any_popup', 10 );
 		Options::set_option( $key, $option );
 
 		$this->assertEquals(
 			$option,
 			Options::get_option( $key )
 		);
+	}
+
+	public function test_only_returns_defined_options() {
+		$key    = 'foo';
+		$option = 'bar';
+		Options::set_option( $key, $option );
+
+		$this->expectException( \Exception::class );
+		Options::get_option( $key );
 	}
 }
