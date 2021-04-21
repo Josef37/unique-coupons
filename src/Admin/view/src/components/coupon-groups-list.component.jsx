b9c46bc038c5ff2d515906ff357c81b542abd769
Josef Wittmann <josef.wittmann@tutanota.com>
2021-04-21 20:07:01 +0200

    Don't use action table for coupon group list

--- src/Admin/view/src/components/coupon-groups-list.component.jsx
+++ src/Admin/view/src/components/coupon-groups-list.component.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectCouponGroupsIds } from "../redux/coupons.slice"
-import ActionTable from './action-table.component';
-import CouponGroupRow from './coupon-group-row.component';
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCouponGroupsIds } from "../redux/coupons.slice";
+import CouponGroupRow from "./coupon-group-row.component";
+import { RowContainer } from "./action-table.component";
+import Paper from "@material-ui/core/Paper";
 
 const CouponGroupsList = () => {
-	const couponGroupIds = useSelector(selectCouponGroupsIds)
+	const couponGroupIds = useSelector(selectCouponGroupsIds);
 
-	return <ActionTable
-		ids={couponGroupIds}
-		Row={({ id }) => <CouponGroupRow groupId={id} />}
-	/>
-}
+	return (
+		<Paper>
+			{couponGroupIds.map((id) => (
+				<RowContainer key={id}>
+					<CouponGroupRow groupId={id} />
+				</RowContainer>
+			))}
+		</Paper>
+	);
+};
 
 export default CouponGroupsList;
