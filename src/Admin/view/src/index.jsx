8e56ed1b3945bd68c7273a4d5c41cd9881aee7a8
Josef Wittmann <josef.wittmann@tutanota.com>
2021-04-21 23:00:57 +0200

    Add another container to shadow root
    Material UI Modals need a parent for the custom container.
    This parent was null in the previous setup, because it was the shadow root.

--- src/Admin/view/src/index.jsx
+++ src/Admin/view/src/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import { Provider as StoreProvider } from "react-redux";
-import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
-import { StylesProvider } from '@material-ui/core/styles';
-import App from './App';
-import store from './redux/store';
-import { jss, appRoot } from './configure-shadow-dom';
+import ScopedCssBaseline from "@material-ui/core/ScopedCssBaseline";
+import { StylesProvider } from "@material-ui/core/styles";
+import App from "./App";
+import store from "./redux/store";
+import { jss, appRoot } from "./configure-shadow-dom";
 
 ReactDOM.render(
 	<React.StrictMode>
