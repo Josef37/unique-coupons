a3b07d8b8fedb7a3ccd307cb53403df6915256a7
Josef Wittmann <josef.wittmann@tutanota.com>
2021-04-21 20:07:27 +0200

    Format action table with prettier

--- src/Admin/view/src/components/action-table.component.jsx
+++ src/Admin/view/src/components/action-table.component.jsx
@@ -1,77 +1,86 @@
-import React, { useState } from 'react';
-import { styled } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper'
-import Checkbox from '@material-ui/core/Checkbox'
+import React, { useState } from "react";
+import { styled } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Checkbox from "@material-ui/core/Checkbox";
 
 const ActionTable = ({ ids, Row, isFetching }) => {
-	const [selectedIds, setSelectedIds] = useState(new Set())
+	const [selectedIds, setSelectedIds] = useState(new Set());
 
-	const isSelected = id => selectedIds.has(id)
+	const isSelected = (id) => selectedIds.has(id);
 
-	const toggleRow = id => setSelectedIds(selectedRows => {
-		isSelected(id) ? selectedRows.delete(id) : selectedRows.add(id)
-		return new Set(selectedRows)
-	})
+	const toggleRow = (id) =>
+		setSelectedIds((selectedRows) => {
+			isSelected(id) ? selectedRows.delete(id) : selectedRows.add(id);
+			return new Set(selectedRows);
+		});
 
-	const allIdsSelected = () => ids.length === selectedIds.size
-	const noIdsSelected = () => 0 === selectedIds.size
-	const indeterminateSelection = () => !allIdsSelected() && !noIdsSelected()
+	const allIdsSelected = () => ids.length === selectedIds.size;
+	const noIdsSelected = () => 0 === selectedIds.size;
+	const indeterminateSelection = () => !allIdsSelected() && !noIdsSelected();
 
-	const selectAllIds = () => setSelectedIds(new Set(ids))
-	const deselectAllIds = () => setSelectedIds(new Set())
-	const toggleAllIds = () => allIdsSelected() ? deselectAllIds() : selectAllIds()
+	const selectAllIds = () => setSelectedIds(new Set(ids));
+	const deselectAllIds = () => setSelectedIds(new Set());
+	const toggleAllIds = () =>
+		allIdsSelected() ? deselectAllIds() : selectAllIds();
 
-	return <Container>
-		<HeaderRow>
-			<Checkbox
-				checked={allIdsSelected() && !noIdsSelected()}
-				indeterminate={indeterminateSelection()}
-				onChange={toggleAllIds}
-				aria-label={(allIdsSelected() ? "deselect" : "select") + " all rows"}
-			/>
-		</HeaderRow>
-		{isFetching && <RowContainer>Loading entries</RowContainer>}
-		{ids.length === 0
-			? <RowContainer>No entries</RowContainer>
-			: ids.map(id => <RowContainer key={id}>
+	return (
+		<Container>
+			<HeaderRow>
 				<Checkbox
-					checked={isSelected(id)}
-					onChange={() => toggleRow(id)}
-					aria-label={(isSelected(id) ? "deselect" : "select") + " this row"}
+					checked={allIdsSelected() && !noIdsSelected()}
+					indeterminate={indeterminateSelection()}
+					onChange={toggleAllIds}
+					aria-label={(allIdsSelected() ? "deselect" : "select") + " all rows"}
 				/>
-				<Row id={id} />
-			</RowContainer>)
-		}
-	</Container>
-}
+			</HeaderRow>
+			{isFetching && <RowContainer>Loading entries</RowContainer>}
+			{ids.length === 0 ? (
+				<RowContainer>No entries</RowContainer>
+			) : (
+				ids.map((id) => (
+					<RowContainer key={id}>
+						<Checkbox
+							checked={isSelected(id)}
+							onChange={() => toggleRow(id)}
+							aria-label={
+								(isSelected(id) ? "deselect" : "select") + " this row"
+							}
+						/>
+						<Row id={id} />
+					</RowContainer>
+				))
+			)}
+		</Container>
+	);
+};
 
 const Container = styled(Paper)(({ theme }) => ({
 	marginBottom: theme.spacing(2),
 	borderTopLeftRadius: 0,
 	borderTopRightRadius: 0,
-}))
+}));
 
 const rowStyles = {
 	display: "flex",
 	alignItems: "center",
 	gap: "1em",
-}
+};
 
-const RowContainer = styled("div")({
+export const RowContainer = styled("div")({
 	...rowStyles,
 	padding: "1em",
 	"&:not(:last-child)": {
-		borderBottom: "1px solid #ccc"
+		borderBottom: "1px solid #ccc",
 	},
 	"&:hover": {
-		background: "#eee"
-	}
-})
+		background: "#eee",
+	},
+});
 
 const HeaderRow = styled("div")({
 	...rowStyles,
 	padding: "0.4em 1em",
 	borderBottom: "1px solid #888",
-})
+});
 
-export default ActionTable
+export default ActionTable;
