4f6d49262cdfc3d17d64d1236b6ab10f3817af2a
Josef Wittmann <josef.wittmann@tutanota.com>
2021-05-10 20:35:37 +0200

    Bump version to 0.1.2

--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: coupon, coupons, unique
 Requires at least: 5.3
 Tested up to: 5.7
 Requires PHP: 7.1
-Stable tag: 0.1.1
+Stable tag: 0.1.2
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- unique-coupons.php
+++ unique-coupons.php
@@ -7,13 +7,13 @@
  * Author URI:      https://josefwittmann.dev/
  * Text Domain:     unique-coupons
  * Domain Path:     /languages
- * Version:         0.1.1
+ * Version:         0.1.2
  */
 
 require __DIR__ . '/vendor/autoload.php';
 
 function unique_coupons_load_plugin() {
-	$version         = '0.1.1';
+	$version         = '0.1.2';
 	$plugin_root_dir = plugin_dir_path( __FILE__ );
 	$plugin_root_url = plugin_dir_url( __FILE__ );
 	$loader          = new UniqueCoupons\Loader( $version, $plugin_root_dir, $plugin_root_url );
